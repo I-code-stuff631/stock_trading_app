2fd5a9c1b231f88627b8123d0de1ceac53dac0aa
sadsushifool <ryanjslingerland@gmail.com>
2023-02-24 09:35:39 -0500

    Random order and Order Class

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import random
+
+class Order:
+    def __init__(self, id, price, symbol, timestamp):
+        self.id = id
+        self.price = price
+        self.symbol = symbol
+        self.timestamp = timestamp
+
+
+def random_order() -> Order:
+    return Order(random.randrange(100), random.randint(100, 150), 0, 0)
+
+
 def main():
+    random_order()
     pass
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+

b589f466ef97b65cd23a66743fb161d4bdeffb09
Adrian Metallinos <39782071+I-code-stuff631@users.noreply.github.com>
2023-02-26 13:45:52 -0500

    Made symbol list into an Enum and incoming now yields transactions with a random symbol from said enum

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import random
 from typing import Final
 import time
+from enum import Enum, auto
 
-# My discord is Adrian_Torpedo#7154, if you don't have discord my phone number is 905-933-5563. You should message me
-# on one of them so if you run into problems, or I am doing someting you need to know about we can communicate about it.
 
-# I sent you a friend request on discord!
-
-symbolList = ["AAPL", "TSLA", "MSFT", "GOOGL", "SPOT", "NFLX"]
-start_time = int(time.time())
+class Symbol(Enum):
+    AAPL = auto()
+    TSLA = auto()
+    MSFT = auto()
+    GOOGL = auto()
+    SPOT = auto()
+    NFLX = auto()
 
 
 class Transaction:
 
     # noinspection PyShadowingBuiltins
-    def __init__(self, buy: bool, symbol: str, price, timestamp, id: int):
+    def __init__(self, buy: bool, symbol: Symbol, price, timestamp, id: int):
         self.buy: Final = buy
         self.symbol: Final = symbol
         self.price: Final = price
@@ -27,21 +29,15 @@ def incoming():
     while True:
         yield Transaction(
             random.randint(0, 1) == 1,
-            "",
+            random.choice(list(Symbol)),
             random.uniform(5, 100),
-            time.time() + random.randint(-5, 5),
+            time.time() + random.uniform(-5, 5),
             random.randrange(two_power_128),
         )
 
 
 def main():
     for transaction in incoming():
-        # using end_time - start_time to create a unique timestamp for each transaction
-
-        end_time = int(time.time())
-        final_time = end_time - start_time
-
-        return Transaction(random.randint(0, 1), random.choice(symbolList), random.randint(100, 150), final_time, 0)
         pass
 
 
